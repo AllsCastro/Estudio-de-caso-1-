4136cf1074c35ce14b0546f2021edc250563f0dc
AllsCastro <allisoncastro0515@gmail.com>
2025-02-26 20:56:54 -0600

    Add files via upload

---
+++ src/estudio/de/caso/pkg1/EstudioDeCaso1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package estudio.de.caso.pkg1;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author allis
+ */
+public class EstudioDeCaso1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String vendedor = JOptionPane.showInputDialog("Ingrese el nombre del agente de ventas:");
+        String cedula = JOptionPane.showInputDialog("Ingrese el numero de cedula :");
+        String codigo = JOptionPane.showInputDialog("Ingrese el codigo:");
+        String sucursal = JOptionPane.showInputDialog("Ingrese la sucursal a la que pertenece:");
+        String vehiculo = JOptionPane.showInputDialog("Indique si cuenta con vehiculo :");
+       
+   System.out.println(clientes.company);
+        facturas nombrecliente = new facturas();
+        nombrecliente.facturaCliente1();
+
+        clientes uno = new clientes();
+
+        
+}}
